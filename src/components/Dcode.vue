25f7a9f929a94e62971b99b377ca8401c0545eb1
李梦龙 <649076408@qq.com>
2017-12-06 15:40:49 +0800

    feature: dAcode cpt

---
+++ src/components/Dcode.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="dcode">
+    <pre class="pre"><code class="code" ref="code"><slot></slot></code></pre>
+    <a class="link dcode-handle" href="javascript:;">复制</a>
+    <span class="dcode-type">{{type}}</span>
+  </div>
+</template>
+<script>
+import code from '@/tools/code';
+
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'js',
+    },
+  },
+  mounted() {
+    this.code = this.$refs.code.innerHTML;
+    this.$refs.code.innerHTML = code[this.type](this.code);
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../assets/styles/components/dcode.scss";
+</style>
