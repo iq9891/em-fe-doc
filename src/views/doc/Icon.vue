40f141972ee5faeb788a8d3728039a6f27264e55
李梦龙 <649076408@qq.com>
2017-12-11 20:12:21 +0800

    feature: icon doc

--- src/views/doc/Icon.vue
+++ src/views/doc/Icon.vue
@@ -1,7 +1,7 @@
 <template>
   <article class="icon">
     <h2 class="h2">字体图标</h2>
-    <p class="p">Em-FE 使用的是 <a href="http://www.iconfont.cn/" class="link">iconfont</a>生成的字体。</p>
+    <p class="p">Em-FE 使用的是 <a href="http://www.iconfont.cn/" class="link">iconfont</a> 生成的字体。</p>
     <h4 class="h4">如何使用</h4>
     <p class="p">使用 <code class="code-inline">&lt;emfe-icon /&gt;</code> 组件，指定图标对应的 <code class="code-inline">type</code> 属性，示例代码：</p>
     <d-code type="html" href="http://output.jsbin.com/zopumap">{{ emfe }}</d-code>
@@ -23,6 +23,7 @@
       </emfe-table-body>
     </emfe-table>
     <h4 class="h4">示例</h4>
+    <p class="p">下面是例子，可以搜索，可以复制。点击下面的图标按钮可以直接复制组件代码！！</p>
     <emfe-row :gutter="16">
       <emfe-col className="color" :xl="12" :lg="18" :md="16" :xs="12" span="18">
         <emfe-input v-model="icon" placeholder="搜索君，我只认识英文，如 fabu" className="icon"></emfe-input>
