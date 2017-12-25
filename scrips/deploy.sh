a057bd1c334782ad85592b6d34e8c8acd858bf43
李梦龙 <649076408@qq.com>
2017-12-25 18:35:08 +0800

    fix: deploy

--- scrips/deploy.sh
+++ scrips/deploy.sh
@@ -1,4 +1,4 @@
-cd pages
+mkdir pages && cd pages
 
 git init
 git clone --branch=gh-pages https://github.com/iq9891/em-fe-doc.git
@@ -6,5 +6,9 @@ git clone --branch=gh-pages https://github.com/iq9891/em-fe-doc.git
 ls
 
 cd em-fe-doc
+
+git branch
+
+echo 90
 
 git status
