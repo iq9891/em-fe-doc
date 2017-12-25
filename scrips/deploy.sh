c4f5d36a6421b6eea1f0ed03ac356417f4bcee8d
李梦龙 <649076408@qq.com>
2017-12-25 17:09:02 +0800

    fix: deploy

--- scrips/deploy.sh
+++ scrips/deploy.sh
@@ -2,6 +2,7 @@ cd ./dist
 git init
 git config user.name "$TRAVIS_COMMIT_USER"
 git config user.email "$TRAVIS_COMMIT_EMAIL"
-git add .
+git add -A
 git commit -m "$TRAVIS_COMMIT_MSG"
+echo ${GH_TOKEN}
 git push --force --quiet "https://git@${GH_REF}" gh-pages
