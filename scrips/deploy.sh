2d5e91ba17a5c4b0712bac078b15491fec2fc10f
李梦龙 <649076408@qq.com>
2017-12-25 17:46:35 +0800

    fix: deploy

--- scrips/deploy.sh
+++ scrips/deploy.sh
@@ -4,6 +4,6 @@ git config user.name "$TRAVIS_COMMIT_USER"
 git config user.email "$TRAVIS_COMMIT_EMAIL"
 git add .
 git commit -m "$TRAVIS_COMMIT_MSG"
-git remote add origin "https://git@${GH_REF}"
+git remote add origin "https://${$ROT_TOKEN}@${GH_REF}"
 git pull origin gh-pages
 git push --force origin gh-pages
